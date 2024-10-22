3379c05e2280904289ca9cec567f54912252d889
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-10-22 16:38:59 +0800

    Working on pseudocode

---
+++ oldest_person_using_array.py
@@ -0,0 +1,8 @@
+# ask user to input name and age
+    # if input is invalid
+    # print error message
+# set conditions for valid name and age
+# store name and age into array
+# enter prompt if user wants to enter another input
+    # if yes, ask user again for input
+    # if no, display name and age of oldest person
