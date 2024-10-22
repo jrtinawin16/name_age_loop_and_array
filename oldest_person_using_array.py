72c7ee29180fb64ff82b0ac3bfb0bb8519912676
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-10-22 17:53:15 +0800

    Establishing loops in each information

--- oldest_person_using_array.py
+++ oldest_person_using_array.py
@@ -6,3 +6,15 @@
 # enter prompt if user wants to enter another input
     # if yes, ask user again for input
     # if no, display name and age of oldest person
+
+group_of_persons = {}
+
+# Loop 1 - ask user to enter next name and age
+while True:
+    # Loop 2 - automatically retry if input has error
+    while True:
+        try:
+            name = input("Please input a name: ")
+            
+            # Loop 3 - retry if age is not / exceed 2 digits
+            while True:  
